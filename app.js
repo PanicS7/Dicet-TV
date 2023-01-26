84ef76f48d61a81493e3b24c978f1f5238ea2fa7
sanja2457@gmail.com <sanja2457@gmail.com>
2023-01-26 17:46:50 +0100

    Added images and coorrected started code

--- app.js
+++ app.js
@@ -2,40 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-class Title extends React.Component {
-  render() {
-    return (<h1 className="">{this.props.label}</h1>)
-  }
-}
-
-class Paragraph extends React.Component {
-  render() {
-    return (
-      <p className="">
-      {this.props.text}
-      </p>
-    )
-  }
-}
-
-class Main extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return  (
-      <div className="">
-        <Title label={this.props.title} />
-          <Paragraph text={this.props.text} />
-      </div>
-    )
-  }
+function App() {
+ return (
+   "React app"
+   )
 }
 
 
 ReactDOM.render(
-  <Main title="React" text="Caution: do not look into laser with remaining eye."></Main>,
+  <App/>,
   document.getElementById('react-app')
 );
